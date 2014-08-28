c27d1081ba3669b58aca0b5d021b8bd429afe163
Hack Reactor Students <accounts@hackreactor.com>
2014-08-28 15:41:09 -0700

    Rearranged Files

--- server/db/redditModel.js
+++ server/db/redditModel.js
@@ -1,1 +1,11 @@
-var db = require('../db')
+var db = require('./schema.js');
+
+var RedditPost = db.Model.extend({
+  tableName: 'redditContent',
+  hasTimestamps: false
+})
+
+RedditPost.getUnixTime = function(ts) {
+  var date = new Date(ts*1000);
+  return (date);
+}
