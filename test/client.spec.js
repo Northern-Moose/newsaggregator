613e07fa1cc3700621b55f6d1c4f1d8a418626da
Sean Chen <skypemaster007@gmail.com>
2014-09-01 20:44:15 -0500

    Added some tests to client.spec.js

--- test/client.spec.js
+++ test/client.spec.js
@@ -1,13 +1,48 @@
-describe('Server and database unit testing', function() {
+'use strict';
+
+describe('directives', function() {
   beforeEach(function() {
     // Do something before each test
   });
+});
 
-  it('does something', function() {
-    expect(something).toEqual(something else);
-  });
+describe('services', function() {
+  var factory, http;
 
-  it('does something else', function() {
-    expect(something).toEqual(something else);
+  beforeEach(module('posts'));
+  beforeEach(module('services'));
+  beforeEach(module('function'($factory, $rootScope) {
+    http = $http;
+    factory = $factory('GetPosts', {
+      $http: http
+    });
+  }));
+
+  it('should successfully get posts', inject(function(gotten) {
+    // not sure how to write this test
+  }));
+
+});
+
+describe('posts.controller', function() {
+  var controller, scope; 
+
+  beforeEach(module('posts'));
+  beforeEach(module('services'));
+  beforeEach(module('posts.directive'));
+  beforeEach(inject(function($controller, $rootScope) {
+    scope = $rootScope.$new();
+    controller = $controller('PostsController', {
+      $scope: scope
+    });
+  }));
+
+  it('checks the existence of the getPosts function', inject(function(getPosts) {
+    expect(getPosts).toBeDefined();
+  }));
+
+  it('should call GetPosts on user click', function() {
+    expect($scope.clicked).to.be.a('function');
+    // need something else here
   });
 });
